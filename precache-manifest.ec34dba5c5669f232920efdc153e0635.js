cc3f351f9a262dde7d27dd63903c3d4e576abda7
David <scenaristeur@gmail.com>
2020-08-03 01:47:12 +0200

    review chat.vue & use store + mixins

---
+++ precache-manifest.ec34dba5c5669f232920efdc153e0635.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec72b2ebcf3a1b1e3c2",
+    "url": "/parle/css/app.63c22e22.css"
+  },
+  {
+    "revision": "f092e35c2d5606442b53",
+    "url": "/parle/css/chunk-vendors.3a6c5b3b.css"
+  },
+  {
+    "revision": "f7f8a1ebe68fbd87f4b892eb003370d0",
+    "url": "/parle/index.html"
+  },
+  {
+    "revision": "981be4f101d8a4c80a4d",
+    "url": "/parle/js/about.82bd442e.js"
+  },
+  {
+    "revision": "6ec72b2ebcf3a1b1e3c2",
+    "url": "/parle/js/app.9b6ea3f4.js"
+  },
+  {
+    "revision": "f092e35c2d5606442b53",
+    "url": "/parle/js/chunk-vendors.664da0a1.js"
+  },
+  {
+    "revision": "32ef6ad3a8048c4d07a77c7e23d8f292",
+    "url": "/parle/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/parle/robots.txt"
+  },
+  {
+    "revision": "83dfc112ffad3b778c6f82c9147f94b8",
+    "url": "/parle/vendor/rdflib.min.js"
+  },
+  {
+    "revision": "c46af35a8510690ecdaa6795fa1b78e9",
+    "url": "/parle/vendor/solid-file-client.bundle.js"
+  },
+  {
+    "revision": "942b236b1c9c4b0d70bf8fadc1f38310",
+    "url": "/parle/vendor/solid-query-ldflex.bundle.js"
+  },
+  {
+    "revision": "251b053187b51004112ec6c53a615781",
+    "url": "/parle/vendor/solid-query-ldflex.rdflib.js"
+  }
+]);
