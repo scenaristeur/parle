ed87e98ccbf72c0bf9f6dbd1d5dcb629e56652ac
David <scenaristeur@gmail.com>
2020-08-03 13:45:36 +0200

    sub-channel creation

---
+++ precache-manifest.0d20a55cca66e2562b6b8f269d293143.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2baa695002556d12670f",
+    "url": "/parle/css/app.d4760c28.css"
+  },
+  {
+    "revision": "1a14a0b4ef6f80251564",
+    "url": "/parle/css/chunk-vendors.3a6c5b3b.css"
+  },
+  {
+    "revision": "056187025aca52ebe34b769031bacb80",
+    "url": "/parle/index.html"
+  },
+  {
+    "revision": "e9837c8c5f07bd0b7dfd",
+    "url": "/parle/js/about.3043449c.js"
+  },
+  {
+    "revision": "2baa695002556d12670f",
+    "url": "/parle/js/app.5ae83cd1.js"
+  },
+  {
+    "revision": "1a14a0b4ef6f80251564",
+    "url": "/parle/js/chunk-vendors.f4cf4c4f.js"
+  },
+  {
+    "revision": "32ef6ad3a8048c4d07a77c7e23d8f292",
+    "url": "/parle/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/parle/robots.txt"
+  },
+  {
+    "revision": "83dfc112ffad3b778c6f82c9147f94b8",
+    "url": "/parle/vendor/rdflib.min.js"
+  },
+  {
+    "revision": "c46af35a8510690ecdaa6795fa1b78e9",
+    "url": "/parle/vendor/solid-file-client.bundle.js"
+  },
+  {
+    "revision": "942b236b1c9c4b0d70bf8fadc1f38310",
+    "url": "/parle/vendor/solid-query-ldflex.bundle.js"
+  },
+  {
+    "revision": "251b053187b51004112ec6c53a615781",
+    "url": "/parle/vendor/solid-query-ldflex.rdflib.js"
+  }
+]);
