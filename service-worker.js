7ac1ba8d77a3fbd2e9a85e3b08f3697806e9eb55
David <scenaristeur@gmail.com>
2020-08-04 05:09:16 +0200

    add calendar, and sub-channelscreation & switch

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/parle/precache-manifest.0d20a55cca66e2562b6b8f269d293143.js"
+  "/parle/precache-manifest.a5ae03fb41860030b6ca83c5377a6718.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "parle"});
